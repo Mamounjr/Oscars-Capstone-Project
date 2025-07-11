fb473de59ad097f1dd97e72043faa7a97be9949c
Frimpong Evans Boateng <kb1341899@gmail.com>
2025-07-11 19:22:35 -0400

    Update outputs.tf

--- iac/modules/s3/outputs.tf
+++ iac/modules/s3/outputs.tf
@@ -2,23 +2,3 @@
 output "bucket_name" {
   value = aws_s3_bucket.this.bucket
 }
-
-output "vpc_id" {
-  value = module.network.vpc_id
-}
-
-output "public_subnet_ids" {
-  value = module.network.public_subnet_ids
-}
-
-output "private_subnet_ids" {
-  value = module.network.private_subnet_ids
-}
-
-output "s3_bucket_name" {
-  value = module.s3.bucket_name
-}
-
-output "alb_dns_name" {
-  value = module.loadbalancer.alb_dns_name
-}
