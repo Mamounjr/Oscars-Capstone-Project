206f12c54f925da203ee0d9c6719e23857a4ef38
Frimpong Evans Boateng <kb1341899@gmail.com>
2025-07-11 19:18:54 -0400

    Update main.tf

--- iac/modules/autoscaling/main.tf
+++ iac/modules/autoscaling/main.tf
@@ -1,15 +1,7 @@
-# modules/autoscaling/main.tf
-variable "public_subnet_ids" { type = list(string) }
-variable "private_subnet_ids" { type = list(string) }
-variable "public_sg_id" { type = string }
-variable "private_sg_id" { type = string }
-variable "ec2_profile_name" { type = string }
-
 resource "aws_launch_template" "public" {
   name_prefix   = "public-lt"
   image_id      = "ami-0c94855ba95c71c99"
   instance_type = "t2.micro"
-  iam_instance_profile { name = var.ec2_profile_name }
   vpc_security_group_ids = [var.public_sg_id]
 }
 
@@ -28,7 +20,6 @@ resource "aws_launch_template" "private" {
   name_prefix   = "private-lt"
   image_id      = "ami-0c94855ba95c71c99"
   instance_type = "t2.micro"
-  iam_instance_profile { name = var.ec2_profile_name }
   vpc_security_group_ids = [var.private_sg_id]
 }
 
