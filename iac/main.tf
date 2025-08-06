303901af2e385f2388ffcf5eaa06e82b4237ae4f
Mamounjr <46901925+Mamounjr@users.noreply.github.com>
2025-08-06 14:53:13 -0400

    Adding comments to main.tf
    Adding comments to main.tf

--- iac/main.tf
+++ iac/main.tf
@@ -2,10 +2,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
+#Deploy network infrastructure
 module "network" {
   source = "./modules/network"
 }
 
+#Creating autoscaling group using network resources
 module "autoscaling" {
   source             = "./modules/autoscaling"
   public_subnet_ids  = module.network.public_subnet_ids
@@ -14,6 +16,7 @@ module "autoscaling" {
   private_sg_id      = module.network.private_sg_id
 }
 
+# Create and configure load balancer
 module "loadbalancer" {
   source         = "./modules/loadbalancer"
   public_subnets = module.network.public_subnet_ids
@@ -21,6 +24,7 @@ module "loadbalancer" {
   public_sg_id   = module.network.public_sg_id
 }
 
+#Provision S3 bucket
 module "s3" {
   source = "./modules/s3"
 }
